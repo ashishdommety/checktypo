761748e2c1a887f86039dc17129d69280e066701
ashishucb <ashishuiux@gmail.com>
2018-03-27 11:53:41 -0700

    re-factor cleanCustomArgs

--- helpers/_cleanCustomArgs.js
+++ helpers/_cleanCustomArgs.js
@@ -1,21 +1,26 @@
 /* Function to clean custom arguments */
 
 export default function cleanCustomArgs(arr){
-    let filteredArr = [];
-    let argumentsArr = [];
+  let arr = arr;
+  
+    return function customArgs(){
+      let filteredArr = [];
+      let argumentsArr = [];
 
-    // copy arguments obj to array
-    for(let k=0; k < arguments.length; k++){
-      argumentsArr.push(arguments[k]);
-    }
-
-    // loop through noEmail array
-    for(let j=0; j < arr.length; j++){ 
-      /* If noEmail array element doesn't exist in argument array, push to filtered array */
-      if(!argumentsArr.includes(arr[j])){
-        filteredArr.push(arr[j]);
+      // copy arguments obj to array
+      for(let k=0; k < arguments.length; k++){
+        argumentsArr.push(arguments[k]);
       }
-    }
 
-    return filteredArr;
-  }
+      // loop through noEmail array
+      for(let j=0; j < arr.length; j++){ 
+        /* If noEmail array element doesn't exist in argument array, push to filtered array */
+        if(!argumentsArr.includes(arr[j])){
+          filteredArr.push(arr[j]);
+        }
+      }
+
+      return filteredArr;
+    }
+   
+}
---
+++ helpers/_index.js
@@ -0,0 +1,10 @@
+/* 
+    Current order of function calls
+    - clean typos and spellcheck
+        - clean white spaces
+            - clean empty strings
+                - final spellcheck 
+                    - filter out numbers
+                        - filter out emails
+                            - remove custom arguments
+*/
