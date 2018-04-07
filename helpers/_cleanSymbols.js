af8772b2cd2770daf37a57845e64fab6536bcc83
unknown <ashishuiux@gmail.com>
2018-04-06 21:50:54 -0700

    change order of filteration

--- helpers/_cleanSymbols.js
+++ helpers/_cleanSymbols.js
@@ -2,4 +2,4 @@ const Typo = require("typo-js");
 const dictionary = new Typo('en_US');
 
 /* Function to clean up symbols ,.:\n\t)(?*$- and then check spelling */
-module.exports = cleanSymbols = (arr) => arr.map((word) => word.replace(/[^a-zA-Z0-9 ]/g," "))
+module.exports = cleanSymbols = (arr) => arr.map((word) => word.replace(/[^a-zA-Z0-9 @]/g," "))
--- helpers/_index.js
+++ helpers/_index.js
@@ -1,12 +1,12 @@
 /* 
     Current order of function calls
-    - clean typos and spellcheck
+    - clean typos
         - clean white spaces
             - clean empty strings
-                - final spellcheck 
                     - filter out numbers
                         - filter out emails
-                            - remove custom arguments
+                            - final spellcheck 
+                                - remove custom arguments
 */
 
 const cleanSymbols = require("./_cleanSymbols"),
