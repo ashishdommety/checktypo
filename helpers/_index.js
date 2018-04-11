b850271d7c1363224b0634d52ab817af3b5172bd
ashishucb <ashishuiux@gmail.com>
2018-04-11 15:57:24 -0700

    nuke _cleanEmptyStrings

--- helpers/_index.js
+++ helpers/_index.js
@@ -12,7 +12,6 @@ Order of function calls
 
 const cleanSymbols = require("./_cleanSymbols"),
       cleanSpaces = require("./_cleanSpaces"),
-      cleanEmptyStrings = require("./_cleanEmptyStrings"),
       spellCheck = require("./_spellCheck"),
       cleanNumbers = require("./_cleanNumbers"),
       cleanEmails = require("./_cleanEmails"),
@@ -22,8 +21,7 @@ module.exports = function (contentArray){
         // change arrangment to reflect correct arrangement
         let noSymbols = cleanSymbols(contentArray);
         let noSpaces = cleanSpaces(noSymbols);
-        let noEmpty = cleanEmptyStrings(noSpaces);
-        let spellCheck = finalSpellCheck(noEmpty);
+        let spellCheck = finalSpellCheck(noSpaces);
         let noNums = cleanNumbers(finalCheck);
         let noMails = cleanEmails(noNums);
         
