8ca324da7c6d3d144f425e4abd46ae79c51b4578
unknown <ashishuiux@gmail.com>
2018-04-11 21:02:34 -0700

    add spinner and clear effects

--- main.js
+++ main.js
@@ -2,14 +2,25 @@ const Typo = require("typo-js");
 const dictionary = new Typo('en_US');
 const Nightmare = require("nightmare");
 const nightmare = Nightmare({show:false});
+const Spinner = require("cli-spinner").Spinner;
+const spinner = new Spinner("searching for typos...%s");
+spinner.setSpinnerString('|/-\\');
+const clear = require("clear");
 const checkTypo = require("./helpers/_index");
-// console.log("checkTypo: " + checkTypo);
-console.log("running...");
-nightmare
-  .goto("https://extension.berkeley.edu/international/programs/legal-studies/")
+
+module.exports = function checktypo(url){
+  spinner.start();
+  nightmare
+  .goto(url)
   .evaluate(() => document.querySelector("body").innerText.split(" "))
   .end()
   .then(function(result){
       // console.log(result);
+      spinner.stop();
+      console.log("\n");
+      clear();
       console.log(checkTypo(result));
   })
+}
+
+checktypo("https://extension.berkeley.edu/international/programs/legal-studies/");
