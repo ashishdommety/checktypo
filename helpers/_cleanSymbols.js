77bd0b8a49135f378e2e75a42ca975086f91233e
unknown <ashishuiux@gmail.com>
2018-05-07 12:54:47 -0700

    change prettier indentation and add eslint

--- helpers/_cleanSymbols.js
+++ helpers/_cleanSymbols.js
@@ -1,3 +1,3 @@
 /* Function to clean up symbols */
 module.exports = cleanSymbols = arr =>
-  arr.map(word => word.replace(/[^a-zA-Z0-9 @]/g, " "));
+    arr.map(word => word.replace(/[^a-zA-Z0-9 @]/g, " "));
--- helpers/_spellCheck.js
+++ helpers/_spellCheck.js
@@ -3,7 +3,7 @@ const dictionary = new Typo("en_US");
 
 /* Function to conduct a spellcheck */
 module.exports = function spellCheck(arr) {
-  let finalTypos = []; // holds remaining typos
-  arr.map(x => (dictionary.check(x) ? "" : finalTypos.push(x))); // checks if there is a typo
-  return finalTypos.length ? finalTypos : "typo free!"; // if there are return them, otherwise return 'typo free!'
+    let finalTypos = []; // holds remaining typos
+    arr.map(x => (dictionary.check(x) ? "" : finalTypos.push(x))); // checks if there is a typo
+    return finalTypos.length ? finalTypos : "typo free!"; // if there are return them, otherwise return 'typo free!'
 };
