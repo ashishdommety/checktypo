8cd42eb689740161345a7b49f57de5234e1fa180
unknown <ashishuiux@gmail.com>
2019-09-20 17:25:33 -0700

    npm audit fix packages + return typos instead of console them

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 const Typo = require("typo-js");
-const dictionary = new Typo('en_US');
+const dictionary = new Typo("en_US");
 const Spinner = require("cli-spinner").Spinner;
 const spinner = new Spinner("searching for typos...%s");
-spinner.setSpinnerString('|/-\\');
+spinner.setSpinnerString("|/-\\");
 const clear = require("clear");
 const checkTypo = require("./helpers/_index");
 const cheerio = require("cheerio");
 const request = require("request");
 
-module.exports = function findTypos(url){
+module.exports = function findTypos(url) {
   spinner.start();
-  request(url, function(err,response, html){
-    if(!err){
-      spinner.stop(); 
+  request(url, function(err, response, html) {
+    if (!err) {
+      spinner.stop();
       clear();
       const $ = cheerio.load(html);
-      let webContent = $("body").text().split(" ");
-      console.log(checkTypo(webContent));
+      let webContent = $("body")
+        .text()
+        .split(" ");
+      return checkTypo(webContent);
+      // console.log(checkTypo(webContent));
     }
-  })
-}
-
+  });
+};
